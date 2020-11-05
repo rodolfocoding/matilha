c5a0a52cc75367d2069bcdcc1e3c98e2758c17db
Joao Pedro <dp.joaopedro@gmail.com>
2020-11-05 15:29:15 -0300

    home screen prototype

---
+++ matilha-frontend/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Components
+import { HomeComponent } from './home/home.component';
+
+// Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
+import {MatInputModule} from '@angular/material/input';
+
+const Material = [
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  MatMenuModule,
+  MatDividerModule,
+  MatInputModule,
+];
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, Material],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
