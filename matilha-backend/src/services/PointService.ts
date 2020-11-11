29a1b12e722d201fd3f3a6aa70f6fb3a8e9dc06a
Rodolfo Henrique Silva <rodolfo.silva@HMB.LOCAL>
2020-11-10 22:03:27 -0300

    Merged

--- matilha-backend/src/services/PointService.ts
+++ matilha-backend/src/services/PointService.ts
@@ -19,11 +19,7 @@ class PointService {
   public async execute({ data_ponto, userId }: Request): Promise<Point> {
     const user = await this.pointRepository.create({
       data_ponto,
-<<<<<<< HEAD
       userId: request.token.id,
-=======
-      userId,
->>>>>>> 68b01b0948d6cbe2f6de43f68912760a41022971
     });
 
     return user;
