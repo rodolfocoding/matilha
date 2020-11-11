663cb923f68a248cf5b392c9982de69b957f247c
Joao Pedro <dp.joaopedro@gmail.com>
2020-11-11 17:11:42 -0300

    Added form controller

--- matilha-frontend/src/app/app.module.ts
+++ matilha-frontend/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatDividerModule } from '@angular/material/divider';
 import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 const Material = [
   MatToolbarModule,
@@ -27,7 +28,7 @@ const Material = [
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, Material],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, FormsModule, Material],
   providers: [],
   bootstrap: [AppComponent],
 })
